c89d59499978687d454f6305ed474d8174fc601a
nenad-dinic <nenad.d.dinic01@gmail.com>
2024-07-16 11:34:34 +0200

    create database and dump empty schema

---
+++ database/empty.sql
@@ -0,0 +1,170 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               11.1.2-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.7.0.6850
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for residential_community
+DROP DATABASE IF EXISTS `residential_community`;
+CREATE DATABASE IF NOT EXISTS `residential_community` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `residential_community`;
+
+-- Dumping structure for table residential_community.apartment
+DROP TABLE IF EXISTS `apartment`;
+CREATE TABLE IF NOT EXISTS `apartment` (
+  `apartment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `number` int(10) unsigned NOT NULL,
+  `size` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`apartment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.bill
+DROP TABLE IF EXISTS `bill`;
+CREATE TABLE IF NOT EXISTS `bill` (
+  `bill_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `bill_type_id` int(10) unsigned NOT NULL,
+  `apartment_id` int(10) unsigned NOT NULL,
+  `month` int(11) unsigned NOT NULL,
+  `file_name` varchar(256) NOT NULL,
+  `file_path` varchar(256) NOT NULL,
+  `is_paid` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`bill_id`),
+  KEY `fk_bill_apartment_id` (`apartment_id`),
+  KEY `fk_bill_bill_type_id` (`bill_type_id`),
+  CONSTRAINT `fk_bill_apartment_id` FOREIGN KEY (`apartment_id`) REFERENCES `apartment` (`apartment_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_bill_bill_type_id` FOREIGN KEY (`bill_type_id`) REFERENCES `bill_type` (`bill_type_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.bill_type
+DROP TABLE IF EXISTS `bill_type`;
+CREATE TABLE IF NOT EXISTS `bill_type` (
+  `bill_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  PRIMARY KEY (`bill_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.building
+DROP TABLE IF EXISTS `building`;
+CREATE TABLE IF NOT EXISTS `building` (
+  `building_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `address` varchar(256) NOT NULL,
+  PRIMARY KEY (`building_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.meeting
+DROP TABLE IF EXISTS `meeting`;
+CREATE TABLE IF NOT EXISTS `meeting` (
+  `meeting_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `building_id` int(10) unsigned NOT NULL,
+  `datetime` datetime NOT NULL,
+  `length` int(11) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`meeting_id`),
+  KEY `fk_meeting_building_id` (`building_id`),
+  CONSTRAINT `fk_meeting_building_id` FOREIGN KEY (`building_id`) REFERENCES `building` (`building_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.poll
+DROP TABLE IF EXISTS `poll`;
+CREATE TABLE IF NOT EXISTS `poll` (
+  `poll_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `building_id` int(10) unsigned NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`poll_id`),
+  KEY `fk_poll_building_id` (`building_id`),
+  CONSTRAINT `fk_poll_building_id` FOREIGN KEY (`building_id`) REFERENCES `building` (`building_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.repair
+DROP TABLE IF EXISTS `repair`;
+CREATE TABLE IF NOT EXISTS `repair` (
+  `repair_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `apartment_id` int(10) unsigned NOT NULL,
+  `description` text NOT NULL,
+  `is_repaired` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`repair_id`),
+  KEY `fk_repair_user_id` (`user_id`),
+  KEY `fk_repair_apartment_id` (`apartment_id`),
+  CONSTRAINT `fk_repair_apartment_id` FOREIGN KEY (`apartment_id`) REFERENCES `apartment` (`apartment_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_repair_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.resident
+DROP TABLE IF EXISTS `resident`;
+CREATE TABLE IF NOT EXISTS `resident` (
+  `resident_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `apartment_id` int(10) unsigned NOT NULL,
+  `expires` date NOT NULL,
+  `is_owner` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  PRIMARY KEY (`resident_id`),
+  KEY `fk_resident_user_id` (`user_id`),
+  KEY `fk_resident_apartment_id` (`apartment_id`),
+  CONSTRAINT `fk_resident_apartment_id` FOREIGN KEY (`apartment_id`) REFERENCES `apartment` (`apartment_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_resident_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `role` enum('admin','manager','resident') NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `uq_user_username` (`username`),
+  UNIQUE KEY `uq_user_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table residential_community.vote
+DROP TABLE IF EXISTS `vote`;
+CREATE TABLE IF NOT EXISTS `vote` (
+  `vote_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `result` tinyint(1) unsigned NOT NULL,
+  PRIMARY KEY (`vote_id`),
+  KEY `fk_vote_user_id` (`user_id`),
+  CONSTRAINT `fk_vote_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;

824869fdea72f6424185beb91609b7ee29a8ee47
nenad-dinic <nenad.d.dinic01@gmail.com>
2024-08-01 12:13:57 +0200

    remove sass

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
